b8514d24aac465f823b0e5e6badc1d23319781d2
im4pepper <jon@jonahlquist.com>
2015-06-25 15:56:20 -0600

    revised keydown function code

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     alert('I made the click work');
     });
 
- $('#add-items').keyup(function (e) {
+ $('#add-items').keydown(function (e) {
  	if (e.keyCode == 13) {
  	console.log( 'I made the enter work' );
     alert('I made the enter work');
