490c21e45a592072f7d0455a5743802df852fe83
im4pepper <jon@jonahlquist.com>
2015-06-25 15:54:28 -0600

    keydown function

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
-	
+
  $('#add').on("click", function() {
     console.log( 'I made the click work' );
     alert('I made the click work');
+    });
 
-  });
+ $('#add-items').keyup(function (e) {
+ 	if (e.keyCode == 13) {
+ 	console.log( 'I made the enter work' );
+    alert('I made the enter work');
+ 	}
+ });
 
 });
