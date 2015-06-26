9810c6d842b1ba4a97a09671bbc91a5f8d34523c
im4pepper <jon@jonahlquist.com>
2015-06-25 22:43:07 -0600

    on click new list item function

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@ $(document).ready(function() {
  $('#add').on("click", function() {
     console.log( 'I made the click work' );
     alert('I made the click work');
+    var new_task = $('#add-items').val();
+    $('#list ul').append('<li>'+new_task+'</li>');
+    return false;
     });
 
  $('#add-items').keydown(function (e) {
