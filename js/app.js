978de02859dfd9666bbc80544ba96160af74a8f2
im4pepper <jon@jonahlquist.com>
2015-06-30 09:04:12 -0600

    functioning added item element and revised css

--- js/app.js
+++ js/app.js
@@ -1,21 +1,32 @@
 $(document).ready(function() {
+	var checkButton = '<button type="submit" id="check" name="check"></button>'
+	var removeButton = '<button type="submit" id="added-remove" name="added-remove"></button>'
 
  $('#add').on("click", function() {
-    console.log( 'clicking the + icon works' );
-    /* alert('I made the click work'); */
-    var new_task = $('#add-items').val();
-    $('#list ul').append('<li>'+new_task+'</li>');
-    return false;
+    buildList()
     });
+
+function buildList() {
+    var inputText = $('#add-items').val();
+    var inputSpan = '<span id='+"added-item"+'>'+ inputText +'</span>'
+    var listItem = '<li>'+ checkButton + inputSpan + removeButton +'</li>'
+    $('#list ul').append(listItem);
+    $('#add-items').val('');
+    return false;
+}
 
  $('#add-items').keydown(function (e) {
  	if (e.keyCode == 13) {
+        buildList()
  		/* postItem(); */
- 	console.log( 'keydown the enter key works' );
+ 	console.log('keydown the enter key works');
     /* alert('I made the enter work'); */
  	}
  });
-
+ /*
+ $('#add-items').val();{
+ value = $("#add-items").val(); 
+ }*/
 /*$('#list').on('click', '.delete', function(e){e.preventDefault(); $(this).parent().remove()});*/
 
 });
