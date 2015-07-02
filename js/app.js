efe826a1c7120037d50bd29980135e26c37ecdff
im4pepper <jon@jonahlquist.com>
2015-07-02 11:49:00 -0600

    remove list item function in jquery

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     /* defines list item buttons */
 	var checkButton = '<button type="submit" id="check" name="check"></button>'
-	var removeButton = '<button type="submit" id="added-remove" name="added-remove"></button>'
+	var removeButton = '<button type="submit" id="remove" name="remove"></button>'
 
 /* limits input characters in the list item */
 $('input').attr('maxlength','27');
@@ -10,29 +10,30 @@ $('input').attr('maxlength','27');
 function buildList() {
     var inputText = $('#add-items').val();
     var inputSpan = '<span id='+"added-item"+'>'+ inputText +'</span>' 
-    var listItem = '<li>'+ checkButton + inputSpan + removeButton +'</li>'
+    var listItem = '<li id="myNewItem">'+ checkButton + inputSpan + removeButton +'</li>'
     $('#list ul').append(listItem);
     $('#add-items').val('');
     return false;
 }
 
 /* adds the item to the list when the '+' icon is clicked */
- $('#add').on("click", function() {
+$('#add').on("click", function() {
     buildList()
     console.log('the add item button functions');
-    });
+  });
 
 /* adds the item to the list when the 'enter/return' key is struck */
-  $('#add-items').keydown(function (e) {
+$('#add-items').keydown(function (e) {
     if (e.keyCode == 13) {
     buildList()
     console.log('the enter/return key functions');
     }
  });
 
-  /* removes the item from the list when the 'x' icon is clicked */
- $('#checked-remove').on("click", function() {
+/* removes the item from the list when the 'x' icon is clicked */
+$("ul").on("click", '#remove', function() {
+    $("#myNewItem").remove();
     console.log('the remove item button functions');
-    });
+ });
 
 });
