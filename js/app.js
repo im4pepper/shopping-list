64ec58c919ed86914056166f65cca2b2898a3fc6
im4pepper <jon@jonahlquist.com>
2015-07-07 21:16:50 -0600

    changing dynamically added item id's to class

--- js/app.js
+++ js/app.js
@@ -2,17 +2,17 @@ $(document).ready(function() {
     /* defines list item buttons */
 	var checkButton = '<button class="checked--button" type="submit" id="check" name="check"></button>'
 	var removeButton = '<button class="checked--remove" type="submit" id="remove" name="remove"></button>'
-
+    
 /* limits input characters in the list item */
 $('input').attr('maxlength','28');
 
 /* builds the 'added item' state */
 function buildList() {
-    var inputText = $('#add-items').val();
+    var inputText = $('.add-items').val();
     var inputSpan = '<span class="need--item" id='+"added-item"+'>'+ inputText +'</span>' 
-    var listItem = '<li id="myNewItem">'+ checkButton + inputSpan + removeButton +'</li>'
-    $('#list ul').append(listItem);
-    $('#add-items').val('');
+    var listItem = '<li class="list--item" id="myNewItem">'+ checkButton + inputSpan + removeButton +'</li>'
+    $('.list ul').append(listItem);
+    $('.add-items').val('');
     return false;
 }
 
@@ -23,7 +23,7 @@ $('#add').on("click", function() {
   });
 
 /* adds the item to the list when the 'enter/return' key is struck */
-$('#add-items').keydown(function (e) {
+$('.add-items').keydown(function (e) {
     if (e.keyCode == 13) {
     buildList()
     console.log('the enter/return key functions');
@@ -31,17 +31,17 @@ $('#add-items').keydown(function (e) {
  });
 
 /* removes the item from the list when the 'x' icon is clicked */
-$("ul").on("click", '#remove', function() {
-    $(this).closest("#myNewItem").remove();
+$("ul").on("click", '.checked--remove', function() {
+    $(this).closest(".list--item").remove();
     console.log('the remove item button functions');
  });
 
 /* toggles between checked and unchecked item when the check or reload icon is clicked */
-$("ul").on("click", '#check' ,function() {
-    $('#check').toggleClass("checked--button unchecked--button");
-    $('#remove').toggleClass("checked--remove unchecked--remove");
-    $('#added-item').toggleClass("need--item have--item");
-  /*$('#remove').removeClass("checked--remove").addClass("unchecked--remove");*/
+$("ul").on("click", "#check" ,function() {
+    $("#check").toggleClass("checked--button unchecked--button");
+    $("#remove").toggleClass("checked--remove unchecked--remove");
+    $("#added-item").toggleClass("need--item have--item");
+    /*$('.checked--button').removeClass("checked--button").addClass("unchecked--button");*/
      console.log('the check button functions');     
 });
 
